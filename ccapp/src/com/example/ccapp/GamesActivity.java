23edb119ff328a95caca2c4aa4700bb100b5c820
Mingzhe Lin <mingzlin@seas.upenn.edu>
2013-02-21 15:00:30 +0800

    cfg

--- ccapp/src/com/example/ccapp/GamesActivity.java
+++ ccapp/src/com/example/ccapp/GamesActivity.java
@@ -1,7 +1,23 @@
 package com.example.ccapp;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 
 public class GamesActivity extends Activity {
-
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_games);
+	}
+	
+	public void onFinishButtonClick(View view) {
+		Intent i = new Intent();
+		setResult(RESULT_OK, i);
+		
+		finish();
+	}
 }
