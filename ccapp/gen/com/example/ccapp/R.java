27f4cf012f90ad8374b51ed04d3a39767bb843b5
Jevon Yeoh <jevonyeoh@gmail.com>
2013-02-21 02:15:00 -0500

    pre scroll

--- ccapp/gen/com/example/ccapp/R.java
+++ ccapp/gen/com/example/ccapp/R.java
@@ -21,13 +21,19 @@ public final class R {
         public static final int panic=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_contacts=0x7f030000;
+        public static final int activity_friends=0x7f030001;
+        public static final int activity_games=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_more=0x7f030004;
+        public static final int activity_voice=0x7f030005;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int back=0x7f040004;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
         public static final int welcome=0x7f040003;
--- ccapp/src/com/example/ccapp/FriendsActivity.java
+++ ccapp/src/com/example/ccapp/FriendsActivity.java
@@ -1,10 +1,12 @@
 package com.example.ccapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Toast;
 
 public class FriendsActivity extends Activity {
 	
@@ -19,5 +21,14 @@ public class FriendsActivity extends Activity {
 		setResult(RESULT_OK, i);
 		
 		finish();
+	}
+	
+	public void onPanicButtonClick(View view) {
+		Context context = getApplicationContext();
+		CharSequence text = "You clicked the panic button!";
+		int duration = Toast.LENGTH_SHORT;
+
+		Toast toast = Toast.makeText(context, text, duration);
+		toast.show();
 	}
 }
