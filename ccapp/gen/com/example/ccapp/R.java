51fd6197dcc4fb35ee84a6922281c74ab438df8d
Yeong Wee <ywee@sas.upenn.edu>
2013-02-21 02:22:53 -0500

    home page

--- ccapp/gen/com/example/ccapp/R.java
+++ ccapp/gen/com/example/ccapp/R.java
@@ -14,11 +14,15 @@ public final class R {
         public static final int ccapp_header=0x7f020000;
         public static final int ccapp_panic=0x7f020001;
         public static final int ic_launcher=0x7f020002;
+        public static final int soothing=0x7f020003;
     }
     public static final class id {
         public static final int button=0x7f070001;
-        public static final int menu_settings=0x7f070002;
+        public static final int image=0x7f070004;
+        public static final int linearLayout1=0x7f070002;
+        public static final int menu_settings=0x7f070005;
         public static final int panic=0x7f070000;
+        public static final int scroller=0x7f070003;
     }
     public static final class layout {
         public static final int activity_contacts=0x7f030000;
@@ -36,6 +40,7 @@ public final class R {
         public static final int back=0x7f040004;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int sampletasks=0x7f040005;
         public static final int welcome=0x7f040003;
     }
     public static final class style {
