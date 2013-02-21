79c484458e23ae8ec65be06acfa0180576c06fc0
Jevon Yeoh <jevonyeoh@gmail.com>
2013-02-21 00:31:30 -0500

    headerview

---
+++ ccapp/src/com/example/ccapp/HeaderView.java
@@ -0,0 +1,29 @@
+package com.example.ccapp;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class HeaderView extends View {
+
+	public HeaderView(Context c) {
+		super(c);
+		init();
+	}
+	
+	public HeaderView(Context c, AttributeSet a) {
+		super(c, a);
+		init();
+	}
+	
+	protected void init() {
+		
+	}
+	
+	protected void onDraw(Canvas canvas) {
+		
+	}
+	
+	
+}
